afae7f7cd7e121306e0b99f09887b7f91d6ec3d5
Paul Starodub <amorallex@gmail.com>
2023-01-30 16:58:16 +0400

    Registered models in the admin panel

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from company.models import Worker, Task, Position, TaskType
+
+
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    date_hierarchy = "tasks__deadline"
+    list_editable = ("is_staff",)
+    list_display_links = ("username",)
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "position",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_completed", "priority")
+    list_editable = ("is_completed", "priority")
+    search_fields = ("name",)
+    list_filter = ("priority", "is_completed")
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+admin.site.register(TaskType)
