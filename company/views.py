5a8b3e087f9308f879ccf395b5ff78d3dadf8977
Paul Starodub <amorallex@gmail.com>
2023-02-01 14:26:46 +0400

    Updated company/views.py with detail-view classes

--- company/views.py
+++ company/views.py
@@ -6,6 +6,8 @@ from company.models import TaskType, Position, Worker, Task
 
 
 class IndexView(generic.TemplateView):
+    """Class for viewing the home page on the site"""
+
     template_name = "company/index.html"
 
     def get_context_data(self, **kwargs):
@@ -32,18 +34,52 @@ class IndexView(generic.TemplateView):
 
 
 class TaskTypeListView(generic.ListView):
+    """Class for viewing the list of task types on the site"""
+
     model = TaskType
     template_name = "company/task_types_list.html"
     context_object_name = "task_types_list"
 
 
+class TaskTypeDetailView(generic.DetailView):
+    """Class for viewing the detail information about task type on the site """
+
+    model = TaskType
+    context_object_name = "task_type"
+    template_name = "company/task_type_detail.html"
+
+
 class PositionListView(generic.ListView):
+    """Class for viewing the list of positions on the site"""
+
+    model = Position
+
+
+class PositionDetailView(generic.DetailView):
+    """Class for viewing the detail information about position on the site """
+
     model = Position
 
 
 class TaskListView(generic.ListView):
+    """Class for viewing the list of tasks on the site"""
+
+    model = Task
+
+
+class TaskDetailView(generic.DetailView):
+    """Class for viewing the detail information about task on the site """
+
     model = Task
 
 
 class WorkerListView(generic.ListView):
+    """Class for viewing the list of workers on the site"""
+
     queryset = Worker.objects.select_related("position")
+
+
+class WorkerDetailView(generic.DetailView):
+    """Class for viewing the detail information about worker on the site """
+
+    model = Worker
