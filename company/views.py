8a47d05c291cc2b4b329e011f2ecdfa2d5273200
Paul Starodub <amorallex@gmail.com>
2023-02-04 21:22:22 +0400

    Updated views.py, task_list.html-added NameSearchForm

--- company/views.py
+++ company/views.py
@@ -173,6 +173,26 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     model = Task
     paginate_by = 10
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = NameSearchForm(
+            initial={
+                "model": name
+            }
+        )
+
+        return context
+
+    def get_queryset(self):
+        form = NameSearchForm(self.request.GET)
+        queryset = Task.objects.all()
+
+        if form.is_valid():
+            return queryset.filter(name__icontains=form.cleaned_data["name"])
+
+        return queryset
+
 
 class TaskDetailView(LoginRequiredMixin, generic.DetailView):
     """Class for viewing the detail information about task on the site """
