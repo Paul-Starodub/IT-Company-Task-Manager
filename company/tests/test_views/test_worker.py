3b9b55d1010b96f06f348d30b1c3b00cd527656d
Paul Starodub <amorallex@gmail.com>
2023-02-07 20:03:45 +0400

    Updated test_worker.py

--- company/tests/test_views/test_worker.py
+++ company/tests/test_views/test_worker.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 
-from company.forms import WorkerCreationForm
 from company.models import Position
 
 WORKERS_URL = reverse("company:worker-list")
@@ -130,23 +129,14 @@ class PrivateWorkerTests(TestCase):
             "password2": "worker12test",
             "first_name": "First",
             "last_name": "Last",
-            "position": self.position
+            "position": self.position.id
         }
-        self.form = WorkerCreationForm(form_data)
-        print(self.form.is_valid())
-        print(form_data)
-        # print(self.client)
-        print(self.client.post(reverse("company:worker-create"), data=form_data))
-        # response = self.client.get(reverse("company:worker-create"))
-        # self.assertEqual(response.status_code, 200)
-        # self.client.force_login(self.user)
+
         self.client.post(reverse("company:worker-create"), data=form_data)
-        print(form_data["username"])
-        # self.assertEqual(response.status_code, 200)
-        # new = get_user_model().objects.all()
-        # print(new)
-        new_worker = get_user_model().objects.get(username=form_data["username"])
-        # print(new_worker)
-        # self.assertEqual(new_worker.username, form_data["username"])
-        # self.assertEqual(new_worker.first_name, form_data["first_name"])
-        # self.assertEqual(new_worker.last_name, form_data["last_name"])
+        new_worker = get_user_model().objects.get(
+            username=form_data["username"]
+        )
+
+        self.assertEqual(new_worker.username, form_data["username"])
+        self.assertEqual(new_worker.first_name, form_data["first_name"])
+        self.assertEqual(new_worker.last_name, form_data["last_name"])
