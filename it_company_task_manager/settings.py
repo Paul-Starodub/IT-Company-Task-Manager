61c4d2b3a95c1b94d26b5abbe268bf2be4a6ac0c
Paul Starodub <amorallex@gmail.com>
2023-02-14 20:23:08 +0400

    Added it-company-task-manager.onrender.com to ALLOWED_HOSTS

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "it-company-task-manager-d7aw.onrender.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "it-company-task-manager.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
